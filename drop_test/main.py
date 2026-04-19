33db1a4d730899c9e8b4dee3eec36ba5e1a63d01
elomark0909 <elo.mark0909@gmail.com>
2026-04-19 14:24:37 +0200

    Törésteszt feladat kész

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,9 +1,34 @@
 from pathlib import Path
 
-def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+def min_num_drops(szam1: int, szam2: int) -> int:
+    if szam1 == 0:
 
+        return int("inf")
+    if szam2 == 0:
+
+        return 0
+
+    dp = [0] * (szam1 + 1)
+    t = 0
+
+    while dp[szam1] < szam2:
+        t += 1
+        new_dp = [0] * (szam1 + 1)
+        for i in range(1, szam1 + 1):
+            new_dp[i] = dp[i - 1] + 1 + dp[i]
+        dp = new_dp
+    return t
+
+def main():
+    data = Path("drop_test/input.txt").read_text(encoding="utf-8")
+    for sor in data.splitlines():
+        sor = sor.strip()
+        if not sor:
+            continue
+
+        szam1_str, szam2_str = sor.split(",")
+        szam1, szam2 = int(szam1_str.strip()), int(szam2_str.strip())
+        print(f"min_num_of_drops({szam1,szam2}) => {min_num_drops(szam1, szam2)}")
 
 if __name__ == "__main__":
     main()
