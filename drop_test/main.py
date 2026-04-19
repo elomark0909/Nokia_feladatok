5e382357c8cdbb6e23e8181a816663d83d15bc27
elomark0909 <elo.mark0909@gmail.com>
2026-04-19 13:53:38 +0200

    Mágikus számok feladat kész

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
     print(data, end="")
--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,10 +1,45 @@
 from pathlib import Path
 
+def is_magic_num(szam):
+
+    string = str(szam)
+
+    return string == string[::-1]
+
+def next_magic_num(n):
+
+    next_szam = n + 1
+
+    string = str(next_szam)
+
+    length = len(string)
+
+    half_szam = string[:(length + 1) // 2]
+
+    mirrored = int(half_szam + half_szam[:length // 2][::-1])
+
+    if mirrored > n:
+
+        return mirrored
+
+    half_inc = str(int(half_szam) + 1)
+
+    if len(half_inc) > len(half_szam):
+
+        return int("1" + "0" * length + "1")
+
+    return int(half_inc + half_inc[:length // 2][::-1])
 
 def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    data = Path("magic_numbers/input.txt").read_text(encoding="utf-8")
 
+    for sor in data.splitlines():
+
+        sor = sor.strip()
+
+        szam = eval(sor.replace("^", "**"))
+
+        print(f"next_magic_num({szam}) => {next_magic_num(szam)}")
 
 if __name__ == "__main__":
     main()
